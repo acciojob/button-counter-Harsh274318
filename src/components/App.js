487ec6e9e349ae3244e49dddd2205cf5233a69fa
Harsh Vardhan Pal <hp77335@gmail.com>
2026-01-29 06:43:14 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  function incCount() {
+    setCount(count + 1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Button clicked {count} times</p>
+      <button onClick={incCount}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
